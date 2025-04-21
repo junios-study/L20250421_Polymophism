6b0d18645732a69f1d6e0112e918553556e90d29
junios-study <31557850+junios-study@users.noreply.github.com>
2025-04-21 13:16:30 +0900

    포인터2

--- main.cpp
+++ main.cpp
@@ -6,12 +6,18 @@ using namespace std;
 
 //메모리 구조
 //Code
-//Data
-//Heap
+//Data : Global
+//Heap 
+// int -> 0x333003(1)
 // 
-// PA[int*] -> 0x2222(0x1121)
+// ADD()
+// -------------------
+// PA[int*] -> 0x2222(0x333003)
 // A[int] -> 0x1121(1)
+// Main()
 //Stack
+
+int Global;
 
 int main()
 {
@@ -20,6 +26,10 @@ int main()
 	int* PA = &A;
 
 	cout << *PA << endl;
+
+	PA = new int();
+	*PA = 1;
+	delete PA;
 
 
 	return 0;
