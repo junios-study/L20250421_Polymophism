0211b4db0565538d0a60fa4c15ea1f281cfd2fe7
junios-study <31557850+junios-study@users.noreply.github.com>
2025-04-21 10:56:58 +0900

    STL, iterator

--- main.cpp
+++ main.cpp
@@ -1,90 +1,64 @@
 #include <iostream>
-#include <vector> //container, C#, java, collection
-//STL 
-//Standard Template Library -> 자료 상관 없이 동작
+#include <vector>  //STL 효율성 위해서, 다른데 써먹기 위해, 
+//생성 추가
+#include <list> //추가 삭제 빠른데, 검색 비용이 비싸다.
+#include <map>
+#include <set>
 
-//C#, java
-//Generic
-
-using namespace std;
-
-//Add1, Add2, Add2
-//name mangling
-//overload
-//int Add(int A, int B)
-//{
-//	return A + B;
-//}
-//
-//float Add(float A, float B, int C)
-//{
-//	return  A + B;
-//}
-//
-//char Add(char A, char B)
-//{
-//	return  A + B;
-//}
-
-
-//meta programmingr
-//c++ -> complier ->(template)+ make -> object
-//template <typename T>
-//T Add(T A, T B)
-//{
-//	return A + B;
-//}
-
-class Adder
-{
-public:
-	int Add(int A, int B);
-	float Add(float A, float B);
-	char Add(char A, char B);
-};
-
-//overload
-int Adder::Add(int A, int B)
-{
-	return A + B;
-}
-
-float Adder::Add(float A, float B)
-{
-	return A + B;
-}
-
-char Adder::Add(char A, char B)
-{
-	return A + B;
-}
-
-
-template <typename T>
-class TemplateAdder
-{
-public:
-	T Add(T A, T B);
-};
-
-
-template<typename T>
-T TemplateAdder<T>::Add(T A, T B)
-{
-	return A + B;
-}
+#include <algorithm>
+//STL conatiner, 인터페이스 표준화
 
 
 int main()
 {
-	TemplateAdder<int> A;
-	
-	cout << A.Add(1, 2) << endl;
+	std::vector<int> Numbers;
+
+	std::list<int> NumbersList;
+
+	Numbers.push_back(1);
+	Numbers.push_back(2);
+	Numbers.push_back(3);
+
+	NumbersList.push_back(1);
+	NumbersList.push_back(2);
+	NumbersList.push_back(3);
+
+	auto where = find(Numbers.begin(), Numbers.end(), 3);
+	auto where2 = find(NumbersList.begin(), NumbersList.end(), 3);
+
+	//[][][][][]
+	for (int i = 0; i < Numbers.size(); ++i)
+	{
+		std::cout << Numbers[i] << std::endl;
+	}
+
+	for (auto iter = Numbers.rbegin();
+		iter != Numbers.rend(); iter++)
+	{
+		std::cout << *iter << std::endl;
+	}
+
+	//for (auto Data : Numbers)
+	//{
+	//	std::cout << Data << std::endl;
+	//}
+
+	//[][]        [][]     [][]       [][]
+	//for (int i = 0; i < NumbersList.size(); ++i)
+	//{
+	//	std::cout << NumbersList[i] << std::endl;
+	//}
+
+	for (auto iter = NumbersList.rbegin();
+		iter != NumbersList.rend(); iter++)
+	{
+		std::cout << *iter << std::endl;
+	}
+
+	//for (auto Data : NumbersList)
+	//{
+	//	std::cout << Data << std::endl;
+	//}
 
 	return 0;
 }
-
-
-
-
-
