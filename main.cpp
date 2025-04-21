c1a4663b0c0789aa58f01ec403b2b6249d42cbe1
junios-study <31557850+junios-study@users.noreply.github.com>
2025-04-21 13:19:27 +0900

    포인터 3

--- main.cpp
+++ main.cpp
@@ -9,27 +9,25 @@ using namespace std;
 //Data : Global
 //Heap 
 // int -> 0x333003(1)
+// Monster -> 0x56566()
 // 
 // ADD()
 // -------------------
+// Monster[AMonster*] -> 0x4444(0x56566)
 // PA[int*] -> 0x2222(0x333003)
 // A[int] -> 0x1121(1)
 // Main()
 //Stack
 
-int Global;
+
 
 int main()
 {
-	//OS 요청
-	int A = 1;
-	int* PA = &A;
+	AMonster* Monster = nullptr;
 
-	cout << *PA << endl;
-
-	PA = new int();
-	*PA = 1;
-	delete PA;
+	//생성 삭제를 조절 할수 있다.
+	Monster = new AMonster();
+	delete Monster;
 
 
 	return 0;
